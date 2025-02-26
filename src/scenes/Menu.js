1d0a5f1310ec23f1f69fa80370c8139a54350bd8
Andrea Morales Villegas <moralesa7412@gmail.com>
2025-02-26 14:36:37 -0800

    added menu screen

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,11 +4,13 @@ class Menu extends Phaser.Scene {
     }
 
     preload() {
+        // loading title image
+        this.load.image('menu', './assets/images/titlescreen.png')
 
     }
 
     create() {
-
+        this.add.image(0,0, 'background').setOrigin(0, 0).setDisplaySize(game.config.width, game.config.height);
     }
 
     update() {
